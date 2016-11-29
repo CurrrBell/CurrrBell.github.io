ca86b5cfeb7373a6f665cd5ff2661d2ea353447e
CurrrBell <chrislbellny@gmail.com>
2016-11-29 13:29:15 -0500

    Made the paper look more like paper

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).scroll(function() {
+    $('#main > #hello > #scroll-me').addClass('scroll-hidden', $(this).scrollTop() > 1);
     $('#top-nav').toggleClass('nav-scrolled', $(this).scrollTop() > 50);
     // $nav.css({"display": $(this).scrollTop()>1 ? "block":"none"});
 });
