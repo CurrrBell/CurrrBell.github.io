d679f55aa251d5b2d780dccdd7fc0a5f160d5be5
CurrrBell <chrislbellny@gmail.com>
2016-12-07 13:13:37 -0500

    made all content fit into that paper div

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).scroll(function() {
     $('#main > #hello > #scroll-me').addClass('scroll-hidden', $(this).scrollTop() > 1);
-    $('#top-nav').toggleClass('nav-scrolled', $(this).scrollTop() > 50);
-    // $nav.css({"display": $(this).scrollTop()>1 ? "block":"none"});
+    $('#top-nav').toggleClass('nav-scrolled', $(this).scrollTop() > 80);
 });
