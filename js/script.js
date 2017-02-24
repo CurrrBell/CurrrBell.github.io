0b7a6f9caa69c93fe4dafa2c110fd76a4fedb36f
CurrrBell <chrislbellny@gmail.com>
2017-02-24 15:37:36 -0500

    added sass and a bunch of other shit idk

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).scroll(function() {
     $('#main > #content > #hello > #scroll-me').addClass('scroll-hidden', $(this).scrollTop() > 1);
-    $('#top-nav').toggleClass('nav-scrolled', $(this).scrollTop() > 80);
+    $('#top-nav').toggleClass('nav-scrolled', $(this).scrollTop() > 180);
 });
 
 $(document).ready(function(){
